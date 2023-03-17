b38d23657e0a0fe56187e2fa03747a1c744f18a6
Seba <gonzaleaugusto@gmail.com>
2023-03-17 11:07:47 -0300

    contacto

---
+++ scripts-controllers/validacion-formulario.js
@@ -0,0 +1,5 @@
+const botonEnviar = document.querySelector("[data-contacto__boton-enviar]");
+// console.log(botonEnviar);
+botonEnviar.addEventListener("click", (event) => {
+  // event.preventDefault();
+});
