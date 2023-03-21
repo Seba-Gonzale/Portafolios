a933bc793d5d787e7bed603a847f1c70dc9e1ece
Seba <gonzaleaugusto@gmail.com>
2023-03-21 09:23:36 -0300

    terminamos

--- scripts-controllers/validacion-formulario.js
+++ scripts-controllers/validacion-formulario.js
@@ -1,36 +1,48 @@
+// Obtención de los elementos html necesarios
 const contacto__formulario = document.querySelector(
   "[data-contacto__formulario]"
 );
-
 const inputsFormulario = [...contacto__formulario.querySelectorAll("input")];
 const textAreaHtml = contacto__formulario.querySelector(
   ".contacto__textarea-mensaje"
 );
+const contacto__boton = contacto__formulario.querySelector(
+  ".contacto__boton--enviar"
+);
+const contadorTextarea = contacto__formulario.querySelector(
+  ".contacto__contador"
+);
 
+//  inserción de eventos en los elementos html
 inputsFormulario.push(textAreaHtml);
 
-console.log(inputsFormulario);
-
 inputsFormulario.forEach((input) => {
-  input.addEventListener("blur", (event) => {
-    const input = event.target;
-    // liberamos el valor del input de espacios para que no pase la validación si solo hay espacios vacios
-    input.value = input.value.trim();
-    // A elemntoError se le asigna el elemnto <p> que muestra el error
-    const elementError = contacto__formulario.querySelector(
-      `.${input.classList[0] + "-error"}`
-    );
-    // si no es valido el input se muestra el msj de error
-    if (!input.checkValidity()) {
-      elementError.textContent = inputErrorMessage(input);
-      elementError.style.display = "block";
-    } else {
-      // si el input es valido se oculta el msj de error
-      elementError.style.display = "none";
-    }
-  });
+  input.addEventListener("blur", (event) => validacion(event.target));
 });
 
+contacto__boton.addEventListener("click", () => {
+  inputsFormulario.forEach((input) => validacion(input));
+});
+
+// procedimientos de validacion
+function validacion(input) {
+  // liberamos el valor del input de espacios para que no pase la validación si solo hay espacios vacios
+  input.value = input.value.trim();
+  // A elementError se le asigna el elemnto <p> que muestra el error
+  const elementError = contacto__formulario.querySelector(
+    `.${input.classList[0] + "-error"}`
+  );
+  // si no es valido el input, se muestra el msj de error
+  if (!input.checkValidity()) {
+    elementError.textContent = inputErrorMessage(input);
+    elementError.style.display = "block";
+  } else {
+    // si el input es valido se oculta el msj de error
+    elementError.style.display = "none";
+  }
+}
+
+// Objeto con mensajes personalizados segun el error obtenido
 const messageType = {
   valueMissing: {
     nombre: "El campo no puede estar vacío!",
@@ -50,6 +62,7 @@ const messageType = {
   },
 };
 
+// Corrobora cual fue el error y quien lo tuvo, y pide el mensaje correspondiente para el usuario
 function inputErrorMessage(input) {
   for (errorName in input.validity) {
     if (input.validity[errorName]) {
@@ -59,10 +72,6 @@ function inputErrorMessage(input) {
 }
 
 /* contador de caracteres para textarea */
-
-const contadorTextarea = contacto__formulario.querySelector(
-  ".contacto__contador"
-);
 textAreaHtml.addEventListener("keyup", (event) => {
   contadorTextarea.textContent = 300 - textAreaHtml.value.length;
   if (textAreaHtml.value.length < 300) {
